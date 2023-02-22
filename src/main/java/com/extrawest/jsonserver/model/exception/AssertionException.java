efe756c40ffd39c1dda2f3666601510929043eb0
Oleksandr Ostapchuk <oleksandr.ostapchuk@extrawest.com>
2023-02-22 16:43:01 +0200

    new Validation and Assertion exceptions added

---
+++ src/main/java/com/extrawest/jsonserver/model/exception/AssertionException.java
@@ -0,0 +1,9 @@
+package com.extrawest.jsonserver.model.exception;
+
+public class AssertionException extends RuntimeException {
+
+    public AssertionException(String message) {
+        super(message);
+    }
+
+}
