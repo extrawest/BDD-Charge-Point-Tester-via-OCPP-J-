623d98b3675bd602e97c97b3d420d06121430249
Oleksandr Ostapchuk <oleksandr.ostapchuk@extrawest.com>
2023-02-22 17:01:48 +0200

    max connections logic added

--- src/main/java/com/extrawest/jsonserver/repository/ServerSessionRepository.java
+++ src/main/java/com/extrawest/jsonserver/repository/ServerSessionRepository.java
@@ -1,5 +1,6 @@
 package com.extrawest.jsonserver.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ServerSessionRepository {
@@ -7,7 +8,12 @@ public interface ServerSessionRepository {
 
     UUID getSessionByChargerId(String chargePointId);
 
+    UUID getSessionForWildCard();
+
     String getChargerIdBySession(UUID uuid);
 
     void removeOpenSessionChargerId(UUID uuid);
+
+    List<UUID> getSessionsListExceptGivenChargerId(String chargePointId);
+
 }
--- src/main/java/com/extrawest/jsonserver/repository/impl/ServerSessionRepositoryImpl.java
+++ src/main/java/com/extrawest/jsonserver/repository/impl/ServerSessionRepositoryImpl.java
@@ -1,18 +1,30 @@
 package com.extrawest.jsonserver.repository.impl;
 
+import static com.extrawest.jsonserver.model.emun.ApiErrorMessage.CONNECTION_LIMIT_EXCEEDED;
+import static com.extrawest.jsonserver.model.emun.ApiErrorMessage.ONLY_ONE_CONNECTION_ALLOWED;
+
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import com.extrawest.jsonserver.model.exception.BddTestingException;
 import com.extrawest.jsonserver.repository.ServerSessionRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServerSessionRepositoryImpl implements ServerSessionRepository {
+    @Value("${max.connections.count:1}")
+    private int maxConnectionCount;
     private final Map<UUID, String> chargePointIdsBySessions = new ConcurrentHashMap<>();
 
     @Override
     public void addOpenSessionChargerId(UUID uuid, String chargeId) {
+        if (chargePointIdsBySessions.size() >= maxConnectionCount) {
+            throw new BddTestingException(CONNECTION_LIMIT_EXCEEDED.getValue());
+        }
         chargePointIdsBySessions.put(uuid, chargeId);
     }
 
@@ -31,7 +43,25 @@ public class ServerSessionRepositoryImpl implements ServerSessionRepository {
     }
 
     @Override
+    public UUID getSessionForWildCard() {
+        if (chargePointIdsBySessions.size() > 1) {
+            throw new BddTestingException(ONLY_ONE_CONNECTION_ALLOWED.getValue());
+        }
+        return chargePointIdsBySessions.keySet().stream()
+                .findFirst().orElseThrow(() -> new RuntimeException("No open session. "));
+    }
+
+    @Override
     public void removeOpenSessionChargerId(UUID uuid) {
         chargePointIdsBySessions.remove(uuid);
     }
+
+    @Override
+    public List<UUID> getSessionsListExceptGivenChargerId(String chargePointId) {
+        return chargePointIdsBySessions.entrySet().stream()
+                .filter(s -> Objects.equals(s.getValue(), chargePointId))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
 }
