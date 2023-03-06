8e784522821eb95e19eee8ceb4fba92b6983bba9
Oleksandr Ostapchuk <oleksandr.ostapchuk@extrawest.com>
2023-03-06 17:48:21 +0200

    SetChargingProfileConfirmationBddHandler added

---
+++ src/main/java/com/extrawest/jsonserver/validation/incoming/confirmation/SetChargingProfileConfirmationBddHandler.java
@@ -0,0 +1,47 @@
+package com.extrawest.jsonserver.validation.incoming.confirmation;
+
+import java.util.Collections;
+import java.util.Map;
+import com.extrawest.jsonserver.validation.incoming.IncomingMessageFactory;
+import com.extrawest.jsonserver.validation.incoming.IncomingMessageFieldsAssertionFactory;
+import eu.chargetime.ocpp.model.smartcharging.ChargingProfileStatus;
+import eu.chargetime.ocpp.model.smartcharging.SetChargingProfileConfirmation;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SetChargingProfileConfirmationBddHandler
+        extends IncomingMessageFieldsAssertionFactory<SetChargingProfileConfirmation>
+        implements IncomingMessageFactory<SetChargingProfileConfirmation> {
+    public static final String STATUS_REQUIRED = "status";
+
+    @PostConstruct
+    private void init() {
+        this.requiredFieldsSetup = Map.of(
+                STATUS_REQUIRED, (conf, status) -> {
+                    if (nonEqual(wildCard, status)) {
+                        conf.setStatus(getValidatedEnumValueOrThrow(ChargingProfileStatus.class, status,
+                                STATUS_REQUIRED));
+                    }
+                }
+        );
+
+        this.optionalFieldsSetup = Collections.emptyMap();
+
+        this.assertionFactory = Map.of(
+                STATUS_REQUIRED, (expectedParams, actual) -> compareStringsIncludeWildCard(
+                        expectedParams, actual.getStatus().name(), STATUS_REQUIRED)
+        );
+    }
+
+    @Override
+    public void validateAndAssertFieldsWithParams(Map<String, String> params, SetChargingProfileConfirmation message) {
+        super.validateParamsViaLibModel(params);
+        super.assertParamsAndMessageFields(params, message);
+    }
+
+}
