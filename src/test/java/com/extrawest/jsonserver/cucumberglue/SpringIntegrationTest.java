3f8c2bf1cd770bea7708b80a05f920bf23f4bf70
Oleksandr Ostapchuk <oleksandr.ostapchuk@extrawest.com>
2023-03-13 21:05:17 +0200

    Redundant classes removed(ContextConfiguration)

--- src/test/java/com/extrawest/jsonserver/cucumberglue/SpringIntegrationTest.java
+++ src/test/java/com/extrawest/jsonserver/cucumberglue/SpringIntegrationTest.java
@@ -1,9 +1,6 @@
 package com.extrawest.jsonserver.cucumberglue;
 
-import com.extrawest.jsonserver.config.ApplicationConfiguration;
-import com.extrawest.jsonserver.config.BeanConfig;
 import com.extrawest.jsonserver.config.JsonServerConfig;
-import com.extrawest.jsonserver.config.JsonServerProfileConfig;
 import com.extrawest.jsonserver.cucumber.StepsDefinitionTest;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -12,10 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 
-
 @ComponentScan(value = "com.extrawest.jsonserver")
-@ContextConfiguration(classes = {BeanConfig.class, JsonServerConfig.class, JsonServerProfileConfig.class,
-        ApplicationConfiguration.class})
+@ContextConfiguration(classes = {JsonServerConfig.class})
 @SpringBootTest(classes = StepsDefinitionTest.class)
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources", glue = "com.extrawest.jsonserver.cucumber.StepsDefinitionTest")
